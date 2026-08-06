65dd7c39b1c7f229332a3f7e5dc7a457d3017e73
idclark34 <idclark34@gmail.com>
2026-08-05 21:06:27 -0400

    Revert "feat(site): testimonials section, awaiting real quotes"
    This reverts commit 0c1c061f542507f3bb4b752a400eee224374c107.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { Manifesto } from '@/components/sections/Manifesto';
 import { GroupsFeature } from '@/components/sections/GroupsFeature';
 import { RoutineFeature } from '@/components/sections/RoutineFeature';
 import { VerificationFeature } from '@/components/sections/VerificationFeature';
-import { Testimonials } from '@/components/sections/Testimonials';
 import { Closing } from '@/components/sections/Closing';
 import { Footer } from '@/components/sections/Footer';
 
@@ -19,7 +18,6 @@ export default function Home() {
       <GroupsFeature />
       <RoutineFeature />
       <VerificationFeature />
-      <Testimonials />
       <Closing />
       <Footer />
     </main>
